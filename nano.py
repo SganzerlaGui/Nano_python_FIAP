d33b04fd209ed35e00e1941a44885995e6d76f44
SganzerlaGui <gui.sganzerla2007@gmail.com>
2026-03-25 11:59:56 -0300

    Alguns arquivos

--- nano.py
+++ nano.py
@@ -1,3 +1,5 @@
+
+
 #nome = input("Digite um funcionário: ")
 #
 #empresa = input("Digite a instituição: ")
@@ -54,14 +56,103 @@
 
 #O append agrega o item que foi respondido no input no - invetario[] -
 
-inventario = []
+#inventario = []
+#resposta = "S"
+#while resposta  == "S":
+#    inventario.append(input("Equipamento: "))
+#    inventario.append(float(input("Valor: ")))
+#    inventario.append(int(input("numero serial: ")))
+#    inventario.append(input("Departamento: "))
+#    resposta = input("Digite \"S\" para continuar: ").upper()
+#for elemento in inventario:
+#    print(elemento)
+
+# MULTIPLAS LISTAS E INDICES --------------------------------------------------------------------
+
+# Usar [] para quando for -- LISTA -- (Guardar nomes, arquivos, salario, etc ...)
+
+#equipamentos = []
+#
+#valores = []
+#
+#seriais = []
+#
+#departamentos = []
+#
+#resposta = "S"
+#
+#while resposta == "S":
+#  equipamentos.append(input("Equipamento: "))
+#
+#  valores.append(float(input("Valor: ")))
+#
+#  seriais.append(int(input("Número Serial: ")))
+#
+#  departamentos.append(input("Departamento: "))
+#
+#  resposta = input("Digite \"S\" para continuar ou ENTER para parar: ").upper()
+#
+#busca=input("Digite o nome do equipamento que deseja buscar: ")
+#
+#for indice in range(0,len(equipamentos)):
+#                                                     #Este código percorre uma lista equipamentos e verifica se um valor busca é igual a um dos elementos na lista. Se a condição for verdadeira, o código dentro do bloco if será executado.
+#                                                     #Em resumo, este código é usado para buscar um valor específico na lista equipamentos e executar uma ação se o valor for encontrado.
+#                                                    #A função range(0, len(equipamentos)) gera uma sequência de números de 0 até len(equipamentos) - 1, que é o número total de elementos na lista equipamentos.
+#                                                    #O loop for indice in range(0, len(equipamentos)): itera sobre essa sequência de números e atribui cada número a uma variável chamada indice.
+#                                                    #Em cada iteração do loop, a variável indice recebe o valor do índice atual durante a iteração. Por exemplo, no primeiro itero, indice recebe 0, no segundo itero, indice recebe 1, e assim por diante.
+#                                                    #Dentro do loop for, você pode usar a variável indice para acessar os elementos da lista equipamentos usando o índice atual. Por exemplo, equipamentos[indice] retorna o elemento da lista equipamentos correspondente ao índice atual.
+#
+#  if busca==equipamentos[indice]:
+#    print("Valor..: ", valores[indice])
+#    print("Serial.:", seriais[indice])
+
+
+
+inventario=[]
 resposta = "S"
-while resposta  == "S":
-    inventario.append(input("Equipamento: "))
-    inventario.append(float(input("Valor: ")))
-    inventario.append(int(input("numero serial: ")))
-    inventario.append(input("Departamento: "))
-    resposta = input("Digite \"S\" para continuar: ").upper()
+while resposta == "S":
+  equipamento=[input("Equipamento: "),
+            float(input("Valor: ")),
+            int(input("Número Serial: ")),
+            input("Departamento: ")]
+  inventario.append(equipamento)
+
+  resposta = input("Digite \"S\" para continuar: ").upper()
 
 for elemento in inventario:
-    print(elemento)
+  print("Nome.........: ", elemento[0])
+  print("Valor........: ", elemento[1])
+  print("Serial.......: ", elemento[2])
+  print("Departamento.: ", elemento[3])
+
+busca=input("Digite o nome do equipamento que deseja buscar: ")
+for elemento in inventario:
+  if busca==elemento[0]:
+    print("Valor..: ", elemento[1])
+    print("Serial.:", elemento[2])
+
+depreciacao=input("Digite o nome do equipamento que será depreciado: ")
+for elemento in inventario:
+  if depreciacao==elemento[0]:
+    print("Valor antigo: ", elemento[1])
+    elemento[1] = elemento[1] * 0.9
+    print("Novo valor: ", elemento[1])
+
+serial=int(input("Digite o serial do equipamento que será excluído: "))
+for elemento in inventario:
+  if elemento[2]==serial:
+    inventario.remove(elemento)
+
+for elemento in inventario:
+  print("Nome.........: ", elemento[0])
+  print("Valor........: ", elemento[1])
+  print("Serial.......: ", elemento[2])
+  print("Departamento.: ", elemento[3]) 
+
+valores=[]
+for elemento in inventario:
+  valores.append(elemento[1])
+if len(valores)>0:
+  print("O equipamento mais caro custa: ", max(valores))
+  print("O equipamento mais barato custa: ", min(valores))
+  print("O total de equipamentos é de: ", sum(valores))
